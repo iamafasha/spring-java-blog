e207f04264cf48b2efde468f6877fff06bf8f812
iamafasha <afashaisakiye@gmail.com>
2023-08-12 09:12:36 +0300

    Add email, username, name and password with unique columns

--- src/main/java/com/iamafasha/post/model/Attachment.java
+++ src/main/java/com/iamafasha/post/model/Attachment.java
@@ -1,14 +1,22 @@
 package com.iamafasha.post.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Attachment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(unique = true)
+    public String username;
+
+    @Column(unique = true)
+    public String email;
+
+    public String name;
+
+    public String password;
+
 
 }
