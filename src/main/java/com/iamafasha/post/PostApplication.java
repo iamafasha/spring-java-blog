77a5af8a905f089f89928c6f45e23ba13b98255d
iamafasha <afashaisakiye@gmail.com>
2023-08-12 07:53:51 +0300

    Initial code

---
+++ src/main/java/com/iamafasha/post/PostApplication.java
@@ -0,0 +1,13 @@
+package com.iamafasha.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostApplication.class, args);
+	}
+
+}
